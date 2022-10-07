ede314e542d924d3968d3dcd210ce7df97ca6be0
braverokmc79 <braverokmc79@gmail.com>
2022-10-08 00:31:14 +0900

    11.session 4 - login 1

--- routes/session/index.js
+++ routes/session/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-
 router.get('/count', function (req, res, next) {
     if (req.session.count) {
         req.session.count++;
@@ -11,10 +10,15 @@ router.get('/count', function (req, res, next) {
     res.render('session/count', { msg: req.session.count });
 });
 
-
 router.get('/tmp', function (req, res, next) {
     res.json("result : " + req.session.count);
 })
+
+
+router.get('/auth/login', function (req, res, next) {
+    res.render("session/login");
+});
+
 
 
 module.exports = router;
