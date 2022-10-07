262c75f5d180537c6804a94cfe2dc6ab7f23f427
braverokmc79 <braverokmc79@gmail.com>
2022-10-07 21:02:58 +0900

    4. cookie 3 - 쇼핑카트 1

--- routes/cookies/index.js
+++ routes/cookies/index.js
@@ -1,15 +1,37 @@
 var express = require('express');
 var router = express.Router();
 
-let count = 0;
-router.get('/', function (req, res, next) {
-    if (req.cookies.count) {
-        count = Number(req.cookies.count);
-    }
+
+router.get('/count', function (req, res, next) {
+    let count = 0;
+    if (req.cookies.count) count = Number(req.cookies.count);
+
     count += 1;
     res.cookie("count", count);
-    res.render('cookies/index', { count: count });
+    res.render('cookies/count', { count: count });
 });
+
+
+const products = [
+    { id: 1, title: 'The history of web' },
+    { id: 2, title: 'The next web' }
+]
+
+router.get("/products", function (req, res, next) {
+    let output = '<ul>';
+    for (let item in products) {
+        output += `<li>
+            <a href="/cookies/products/${products[item].id}">${products[item].title}</a>
+        </li>`;
+    }
+    output += '</ul>';
+    res.render('cookies/proudcts', { output: output, products: products });
+});
+
+
+
+
+
 
 module.exports = router;
 
