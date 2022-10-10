5729d5c9ebf830459c676c15bafb353b767a3360
braverokmc79 <braverokmc79@gmail.com>
2022-10-11 00:22:58 +0900

    21. Security Password 4 - crypto.pbkdf2

--- routes/session/index.js
+++ routes/session/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const md5 = require('md5');
 const sha256 = require('sha256');
-
+const crypto = require('crypto');
 
 router.get('/count', function (req, res, next) {
     if (req.session.count) {
@@ -27,7 +27,7 @@ router.post('/auth/login', function (req, res, next) {
     let user = [
         {
             username: 'egoing',
-            password: 'a6c44b2d30bf9a5893c099bacb94606ca119b38e1454614dc1c069e8ffa958e1',
+            password: '50c1cc27d83110d7bf94dbd9b6f8d5a3b7a4e209fb0d98bc27efadc12ec7eed6f9af421a49dd25bc7700f8552dcdb35fdff57077d5074f74a60fcf60c258fdaa',
             displayName: 'Egoing',
             salt: "@#@#$SDA%#a213"
         },
@@ -42,18 +42,32 @@ router.post('/auth/login', function (req, res, next) {
     const uname = req.body.username;
     const pwd = req.body.password;
     //password :1111
-    console.log("req.body : ", sha256(pwd + user[1].salt));
 
 
-    if (uname === user[0].username && sha256(pwd + user[0].salt) === user[0].password) {
-        //세션 저장
-        req.session.displayName = user[0].displayName;
-        req.session.save(function () {
-            res.redirect("/session/welcome");
-        })
-    } else {
-        res.render("session/login", { error: "아이디 또는 비밀번호가 일치하지 않습니다." });
-    }
+    crypto.pbkdf2(pwd, user[1].salt, 100000, 64, 'sha512', (err, derivedKey) => {
+        if (err) throw err;
+        // Printing the derived key
+        console.log(" derivedKey : ", derivedKey);
+        //출력결과  <Buffer 50 c1 cc 27 d8 31 10 d7 bf 94 db d9 b6 f8 d5 a3 b7 a4 e2 09 fb 0d 98 bc 27 ef ad c1 2e c7 ee d6 f9 af 42 1a 49 dd 25 bc 77 00 f8 55 2d cd b3 5f df f5
+        console.log("Key Derived: ", derivedKey.toString('hex'));
+        //해싱함수를 hex 변환 출력 결과 => 50c1cc27d83110d7bf94dbd9b6f8d5a3b7a4e209fb0d98bc27efadc12ec7eed6f9af421a49dd25bc7700f8552dcdb35fdff57077d5074f74a60fcf60c258fdaa
+
+
+        if (uname === user[0].username && derivedKey.toString('hex') === user[0].password) {
+            //세션 저장
+            req.session.displayName = user[0].displayName;
+            req.session.save(function () {
+                res.redirect("/session/welcome");
+            })
+        } else {
+            res.render("session/login", { error: "아이디 또는 비밀번호가 일치하지 않습니다." });
+        }
+
+    });
+
+
+
+
 })
 
 router.get("/auth/logout", function (req, res, next) {
